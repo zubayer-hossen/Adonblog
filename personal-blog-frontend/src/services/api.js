5abd3c39d132b1e8377c7ef4346bb4ae96aa1832
Zubayer Hossen <fzhossen143r@gmail.com>
2025-06-23 03:34:07 +0600

    second

--- personal-blog-frontend/src/services/api.js
+++ personal-blog-frontend/src/services/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://adonblog.onrender.com',
+  withCredentials: true
 });
+REACT_APP_API_URL=https://adonblog.onrender.com
+
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
